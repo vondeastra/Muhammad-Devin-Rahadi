e9103909a03b9bc1693668cd03a8cb03cbf8ae60
Muhammad Devin Rahadi <144184599+vondeastra@users.noreply.github.com>
2023-12-01 16:34:52 +0700

    Add files via upload

---
+++ JOBSHEET 2/JOBSHEET 2 SOAL 1.C
@@ -0,0 +1,16 @@
+//Created by 23343076_MUHAMMAD DEVIN RAHADI
+#include <stdio.h>
+
+int main(){
+   // deklarasi variabel
+   char nama[22];
+   
+   // input nama user
+   printf("Tuliskan nama mu : ");
+   fgets(nama, sizeof(nama), stdin);
+   
+   // output dari nama user
+   printf("Selamat datang %s dalam pemograman C", nama); 
+
+	return 0;
+}
