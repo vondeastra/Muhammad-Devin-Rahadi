f5e4e31deec2a1692808ca3117996221ce3a844b
Muhammad Devin Rahadi <144184599+vondeastra@users.noreply.github.com>
2023-12-01 16:39:58 +0700

    Add files via upload

---
+++ JOBSHEET 3/JOBSHEET 3 soal 1.c
@@ -0,0 +1,23 @@
+// Tugas Muhammad Devin Rahadi 23343076
+#include <stdio.h>
+
+// tetapkan nilai tetap
+#define x 14250
+
+int main()
+{
+   // Deklarasi variabel 
+     float rupiah, dollar;
+
+   // user menginputkan nilai rupiah
+       printf("masukkan nilai Rupiah: Rp.");
+       scanf("%f", &rupiah);
+
+   // Proses konversi nilai rupiah ke dollar
+     dollar = rupiah / x;
+
+   // Output hasil convert 
+       printf("nilai Dollar adalah: $%.2f\n", dollar);
+
+  return 0;
+}
